79da2f8c4fdf668a550ce785b6cbd5f36916f8d5
Davdus <davlus143@gmail.com>
2018-01-07 17:59:44 +0100

    Relation-Klasse erstellt

---
+++ RelatoGraph/RelatoGraph/Relation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelatoGraph
+{
+    class Relation
+    {
+        private List<int> set1;
+        private List<int> set2;
+        private List<int> graph;
+
+        public List<int> Set1
+        {
+            get { return set1; }
+            set
+            {
+
+            }
+        }
+
+        public List<int> Set2
+        {
+            get { return set2; }
+            set
+            {
+                
+            }
+        }
+
+        public List<int> Graph
+        {
+            get { return graph; }
+            set
+            {
+
+            }
+        }
+
+        public Relation(List<int> set1, List<int> set2, List<int> graph)
+        {
+            
+        }
+
+
+    }
+}
